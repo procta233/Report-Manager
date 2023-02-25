8e7925016dfe6d95188b854d5763e37f43ef658e
procta.233 <ritwik707@gmail.com>
2023-02-25 10:14:31 -0800

    beta version 1.0

--- src/components/admincomponents/ReportTypeSelect.js
+++ src/components/admincomponents/ReportTypeSelect.js
@@ -1,21 +1,27 @@
-import React, { useState, useEffect } from 'react';
-import "./ReportTypeSelect.css"
+import React, { useState, useEffect } from "react";
+import "./ReportTypeSelect.css";
 
 const ReportTypeSelect = () => {
   const [data, setData] = useState([]);
-  const [selectedFormType, setSelectedFormType] = useState('');
-  const [newHead1, setNewHead1] = useState('');
-  const [newHead2, setNewHead2] = useState('');
-  const [newUnit, setNewUnit] = useState('');
-  const [newAttributetype,setNewAttributetype]=useState('');
-  const [atlist,setAtlist]=useState([]);
+  const [selectedFormType, setSelectedFormType] = useState("");
+  const [newHead1, setNewHead1] = useState("");
+  const [newHead2, setNewHead2] = useState("");
+  const [newUnit, setNewUnit] = useState("");
+  const [newAttributetype, setNewAttributetype] = useState("");
+  const [atlist, setAtlist] = useState([]);
+  const [select, setSelect] = useState(false);
+  const [select2, setSelect2] = useState(true);
+  const [select3,setSelect3] = useState(false);
+  const [addReportType, setAddReportType] = useState("");
+  const [newData,setNewData]=useState([]);
   const API = "https://create-users.onrender.com/api/sensorlist";
-  const API2='https://create-users.onrender.com/api/pwd_auto/columns';
+  const API2 = "https://create-users.onrender.com/api/pwd_auto/columns";
+  const API3 ="https://create-users.onrender.com/api/addsensorlist";
 
   useEffect(() => {
-    fetchData(API).then(data => setData(data));
-    fetchAttribute(API2).then(data2 =>setAtlist(data2.slice(2)));
-      
+    fetchData(API).then((data) => setData(data));
+    console.log(data);
+    fetchAttribute(API2).then((data2) => setAtlist(data2.slice(2)));
   }, []);
   const fetchAttribute = async (API2) => {
     // Fetch the data from your API or any source
@@ -31,95 +37,205 @@ const ReportTypeSelect = () => {
     return data;
   };
 
-  const getUniqueFormTypes = () => {
-    const formTypes = data.map(item => item.reporttype);
-    return [...new Set(formTypes)];
-  };
+  const getUniqueFormTypes = [...new Set(data.map((item) => item.reporttype))];
 
   const getFilteredData = () => {
     if (!selectedFormType) {
       return data;
     }
-    return data.filter(item => item.reporttype === selectedFormType);
+    return data.filter((item) => item.reporttype === selectedFormType);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const newId = data.length + 1;
-    const newData = [...data, { id: newId, head1: newHead1, head2: newHead2, unit: newUnit, attributetype: newAttributetype, reporttype: selectedFormType }];
-    setData(newData);
-    setNewHead1('');
-    setNewHead2('');
-    setNewUnit('');
+ 
+    setNewData ([
+      ...newData,
+      {
+        
+        head1: newHead1,
+        head2: newHead2,
+        unit: newUnit,
+        attributetype: newAttributetype,
+        reporttype: selectedFormType,
+      }
+    ]);
+    const newData2 =[
+      ...data,
+      {
+        sensorname: '1',
+        head1: newHead1,
+        head2: newHead2,
+        unit: newUnit,
+        attributetype: newAttributetype,
+        reporttype: selectedFormType,
+      }
+    ];
+    setData(newData2);
+    setNewHead1("");
+    setNewHead2("");
+    setNewUnit("");
+    setNewAttributetype('');
+  
   };
+  const handleSubmit2=async (e)=>{
+    e.preventDefault();
+    console.log(newData);
+    try {
+      const response = await fetch(API3, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newData),
+      });
 
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      console.log("User registration successful");
+      // replace with your logic to handle successful registration
+    } catch (error) {
+      console.error("There was an error registering the user:", error);
+      // replace with your logic to handle registration errors
+    };
+   
+    window.location.reload(false);
+  };
+  const call = (e) => {
+    setSelectedFormType(e);
+    setSelect(true);
+    setSelect2(false);
+    setSelect3(false);
+  };
+  const call2 =()=>{
+    setSelectedFormType(addReportType);
+    setSelect(true);
+    setSelect2(false);
+    setSelect3(false);
+
+  };
+  const addReport =(e)=>{
+    console.log(e);
+    setAddReportType(e);
+  }
   return (
     <div>
-      <select onChange={(e) => setSelectedFormType(e.target.value)}>
-        <option value="">Select a form type</option>
-        {getUniqueFormTypes().map(formType => (
-          <option key={formType} value={formType}>
-            {formType}
-          </option>
-        ))}
-      </select>
-      <table>
-        <thead>
-          <tr>
-            <th>Head1</th>
-            <th>Head2</th>
-            <th>Unit</th>
-            <th>Attribute</th>
-          </tr>
-        </thead>
-        <tbody>
-          {getFilteredData().map(item => (
-            <tr key={item.id}>
-              <td>{item.head1}</td>
-              <td>{item.head2}</td>
-              <td>{item.unit}</td>
-              <td>{item.attributetype}</td>
-            </tr>
-          ))}
-        </tbody>
-        < tfoot >
-        <tr>
-          <th>Head1</th>
-          <th>Head2</th>
-          <th>Unit</th>
-          <th>Attribute</th>
-        </tr>
-        <tr>
-        <td>
-        
-          <input type="text" value={newHead1} onChange={(e) => setNewHead1(e.target.value)} required />
-        </td>
-        <td>
-        
-          <input type="text" value={newHead2} onChange={(e) => setNewHead2(e.target.value)} required />
-        </td>
-        <td>
-      
-          <input type="text" value={newUnit} onChange={(e) => setNewUnit(e.target.value)} required/>
-        </td>
-        <td>
-          
-          <select type="text"  value={newAttributetype} onChange={(e) => setNewAttributetype(e.target.value) }>
-          <option value="" disabled selected>
-          Select an option
-        </option>
-            {atlist.map((cur,index)=>(
-              <option  key={index} >{cur}</option>
-            ))}
-          </select>
-        </td>
-        </tr>
-        
-      </tfoot>
-    
-      </table>
-      <button onClick={handleSubmit}>Add Row</button>
-   
+      {select2 && (
+        <div>
+          <table className="table1">
+            <thead>
+              <tr>
+                <th>S.no.</th>
+                <th>Form Type</th>
+              </tr>
+            </thead>
+            <tbody>
+              {getUniqueFormTypes.map((cur, index) => (
+                <tr key={index} value={cur} onClick={() => call(cur)}>
+                  <td>{index + 1}</td>
+                  <td>{cur}</td>
+                </tr>
+              ))}
+               <tr><button onClick={()=>{setSelect3(true)}}>Add New ReportType</button></tr>
+               {select3&&(
+                <div>  
+                <input
+                type="text"
+                
+                onChange={(e) => addReport(e.target.value)}
+                required
+              />
+              <button onClick={()=>call2()}>Submit</button>
+              </div>
+               )
+
+               }
+            </tbody>
+            
+          </table>
+         
+         
+        </div>
+      )}
+      {select && (
+        <div className="popup">
+          <div>
+            <h2>{selectedFormType} Report</h2>
+          </div>
+          <table>
+            <thead>
+              <tr>
+                <th>Head1</th>
+                <th>Head2</th>
+                <th>Unit</th>
+                <th>Attribute</th>
+              </tr>
+            </thead>
+            <tbody>
+              {getFilteredData().map((item) => (
+                <tr key={item.id}>
+                  <td>{item.head1}</td>
+                  <td>{item.head2}</td>
+                  <td>{item.unit}</td>
+                  <td>{item.attributetype}</td>
+                </tr>
+              ))}
+            </tbody>
+            <tfoot>
+              <tr>
+                <th>Head1</th>
+                <th>Head2</th>
+                <th>Unit</th>
+                <th>Attribute</th>
+              </tr>
+              <tr>
+                <td>
+                  <input
+                    type="text"
+                    value={newHead1}
+                    onChange={(e) => setNewHead1(e.target.value)}
+                    required
+                  />
+                </td>
+                <td>
+                  <input
+                    type="text"
+                    value={newHead2}
+                    onChange={(e) => setNewHead2(e.target.value)}
+                    required
+                  />
+                </td>
+                <td>
+                  <input
+                    type="text"
+                    value={newUnit}
+                    onChange={(e) => setNewUnit(e.target.value)}
+                    required
+                  />
+                </td>
+                <td>
+                  <select
+                    type="text"
+                    value={newAttributetype}
+                    onChange={(e) => setNewAttributetype(e.target.value)}
+                  >
+                    <option value="" disabled selected>
+                      Select an option
+                    </option>
+                    {atlist.map((cur, index) => (
+                      <option key={index}>{cur}</option>
+                    ))}
+                  </select>
+                </td>
+              </tr>
+            </tfoot>
+          </table>
+          <button onClick={handleSubmit}>Add Row</button>
+          <button onClick={handleSubmit2}>Submit</button>
+        </div>
+      )}
     </div>
   );
 };
--- src/components/creatorcomponents/CreateReport.js
+++ src/components/creatorcomponents/CreateReport.js
@@ -1,25 +1,235 @@
-import React from 'react'
-import { Link,Outlet } from 'react-router-dom'
+import { useState, useEffect } from "react";
 
+function CreateReport() {
+  const [values, setValues] = useState({
 
-const CreateReport = () => {
+    clientid: "",
+    reporttype: "",
+    system: "",
+    manufacturer: "",
+    datebegin: "",
+    timebegin: "",
+    dateend: "",
+    timeend: "",
+    timetype: "",
+  });
+  // const [manList, setManList] = useState([]);
+  const [clientList, setClientList] = useState([]);
+  const [systemList, setSystemList] = useState([]);
+  const [repList, setRepList] = useState([]);
+  const API7 = "https://create-users.onrender.com/api/sensorlist";
+  const API5 = "https://create-users.onrender.com/api/clients";
+  // const API4 ="https://create-users.onrender.com/api/manufacturers";
+  const API6 = "https://create-users.onrender.com/api/systems";
+  const API8 = "https://create-users.onrender.com/api/description";
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+ 
+    console.log(values);
+    try {
+      const response = await fetch(API8, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(values),
+      });
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      console.log("User registration successful");
+      // replace with your logic to handle successful registration
+    } catch (error) {
+      console.error("There was an error registering the user:", error);
+      // replace with your logic to handle registration errors
+    };
+   
+    window.location.reload(false);
+  };
+  const formattedDateBegin = values.datebegin.split("/").reverse().join("-");
+  const formattedDateEnd = values.dateend.split("/").reverse().join("-");
+  const getUniqueClients = [
+    ...new Set(clientList.map((item) => item.Client_Id)),
+  ];
+  const getUniqueSystems = [
+    ...new Set(systemList.map((item) => item.System_Name)),
+  ];
+  const getUniqueManu = [
+    ...new Set(systemList.map((item) => item.Manufacturer)),
+  ];
+  const getUniqueFormTypes = [
+    ...new Set(repList.map((item) => item.reporttype)),
+    
+  ];
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setValues({
+      datebegin: formattedDateBegin,
+      dateend: formattedDateEnd,
+    });
+
+    setValues({ ...values, [name]: value });
+  };
+  // const getman = async (API4) => {
+  //   // Fetch the data from your API or any source
+  //   const response = await fetch(API4);
+  //   const data = await response.json();
+  //   return data;
+  // };
+  const getclient = async (API5) => {
+    // Fetch the data from your API or any source
+    const response = await fetch(API5);
+    const data1 = await response.json();
+    console.log(data1);
+    return data1;
+  };
+  const getsys = async (API6) => {
+    // Fetch the data from your API or any source
+    const response = await fetch(API6);
+    const data2 = await response.json();
+    return data2;
+  };
+  const getrep = async (API7) => {
+    // Fetch the data from your API or any source
+    const response = await fetch(API7);
+    const data3 = await response.json();
+
+    return data3;
+  };
+
+  useEffect(() => {
+    // getman(API4).then((data) => setManList(data));
+    getclient(API5).then((data1) => setClientList(data1));
+    getsys(API6).then((data2) => setSystemList(data2));
+    getrep(API7).then((data3) => setRepList(data3));
+  }, []);
+
   return (
-    <div>
-      <h1>create report form is here</h1>
-      <div className="App">
-            <nav>
-            <ul className="App-header">
-              <li>
-                <Link to="/creator/createreport/datetimeoptionality">Select Date and Time Type</Link>
-              </li>
-             
-            </ul>
-            </nav>
-            <Outlet/>
-          </div>
+    <form onSubmit={handleSubmit}>
 
-    </div>
-  )
+
+      <label htmlFor="clientid">Client ID</label>
+      <select
+        type="text"
+        id="clientid"
+        name="clientid"
+        value={values.clientid}
+        onChange={handleChange}
+      >
+        <option value="" disabled selected>
+          Select an option
+        </option>
+        {getUniqueClients.map((cur, index) => (
+          <option key={index}>{cur}</option>
+        ))}
+      </select>
+
+      <label htmlFor="reporttype">Report Type</label>
+      <select
+        type="text"
+        id="reporttype"
+        name="reporttype"
+        value={values.reporttype}
+        onChange={handleChange}
+      >
+        <option value="" disabled selected>
+          Select an option
+        </option>
+        {getUniqueFormTypes.map((cur, index) => (
+          <option key={index}>{cur}</option>
+        ))}
+      </select>
+
+      <label htmlFor="system">System</label>
+      <select
+        type="text"
+        id="system"
+        name="system"
+        value={values.system}
+        onChange={handleChange}
+      >
+        <option value="" disabled selected>
+          Select an option
+        </option>
+        {getUniqueSystems.map((cur, index) => (
+          <option key={index}>{cur}</option>
+        ))}
+      </select>
+
+      <label htmlFor="manufacturer">Manufacturer</label>
+      <select
+        type="text"
+        id="manufacturer"
+        name="manufacturer"
+        value={values.manufacturer}
+        onChange={handleChange}
+      >
+        <option value="" disabled selected>
+          Select an option
+        </option>
+        {getUniqueManu.map((cur, index) => (
+          <option key={index}>{cur}</option>
+        ))}
+      </select>
+
+      <label htmlFor="datebegin">Date Begin</label>
+      <input
+        type="date"
+        id="datebegin"
+        name="datebegin"
+        value={values.datebegin}
+        onChange={handleChange}
+      />
+
+      <label htmlFor="timebegin">Time Begin</label>
+      <input
+        type="time"
+        id="timebegin"
+        name="timebegin"
+        value={values.timebegin}
+        onChange={handleChange}
+      />
+
+      <label htmlFor="dateend">Date End</label>
+      <input
+        type="date"
+        id="dateend"
+        name="dateend"
+        value={values.dateend}
+        onChange={handleChange}
+      />
+
+      <label htmlFor="timeend">Time End</label>
+      <input
+        type="time"
+        id="timeend"
+        name="timeend"
+        value={values.timeend}
+        onChange={handleChange}
+      />
+
+      <label htmlFor="timetype">Time Type</label>
+      <select
+        type="text"
+        id="timetype"
+        name="timetype"
+        value={values.timetype}
+        onChange={handleChange}
+      >
+        <option value="" disabled selected>
+          Select an option
+        </option>
+        <option value="1">Date And Time Separate</option>
+        <option value="2">Date And Time Joined</option>
+      </select>
+
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
-
-export default CreateReport
+export default CreateReport;
